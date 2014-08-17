096bb1a1ba63b4b11d5fff0b9cde98c1e5167e4c
David Jander <djander@gmail.com>
2014-08-17 12:46:16 +0200

    gcode.py: Implemented command generator
    Signed-off-by: David Jander <djander@gmail.com>

--- gcode.py
+++ gcode.py
@@ -10,14 +10,25 @@ class GCode(object):
 		self.f = open(filename, "r")
 		self.dim = cfg.settings["num_motors"]
 		self.motor_name = cfg.settings["motor_name"]
-		self.position = self.position_generator()
-		self.pos = [0.0 for x in range(self.dim)]
+		self.pos = {}
+		for m in self.motor_name:
+			self.pos[m] = 0.0
+		self.pos["F"] = 0.0 # Feedrate
+		self.pos["command"] = "position" # Default position command
+		self.commands = self.command_generator()
 
 	def process_M(self, code, args):
-		print "Process M: code =", code, repr(args)
+		if code == 82: # Absolute E codes
+			pass
+		elif code == 104: # Set hotend temperature
+			print "Set temperature:", args[0]
+		elif code == 106: # Set extruder fan speed
+			print "Set extruder fan speed:", args[0]
+		else:
+			print "Unimplemented M: code =", code, repr(args)
 
 	def process_G(self, code, args):
-		if code == 1:
+		if code == 1 or code == 0: # Controlled movement
 			for w in args:
 				if w == ";":
 					return self.pos
@@ -28,18 +39,32 @@ class GCode(object):
 					val = float(w[1:])
 				except ValueError:
 					continue
-				if mn in self.motor_name:
-					idx = self.motor_name.index(mn)
-					self.pos[idx] = val
+				if mn in self.pos:
+					if mn == "F":
+						val = val / 60
+					self.pos[mn] = val
 				else:
 					print "G1 unknown code:", w
 			return self.pos
+		elif code == 21: # Set metric units
+			pass
+		elif code == 28: # Home position
+			print "Home position"
+			# TODO: Implement homing
+		elif code == 90: # Absolute positioning
+			pass
+		elif code == 92: # Set home
+			# TODO: Implement home offsetting
+			pass
 		else:
 			print "Unimplemented G: code =", code, repr(args)
 		return None
 
 	def process_T(self, code, args):
-		print "Process T: code =", code, repr(args)
+		if code == 0: # Tool selection 0
+			pass
+		else:
+			print "Unimplemented T: code =", code, repr(args)
 
 	def process_line(self, cmd, l):
 		words = l.split()
@@ -55,7 +80,7 @@ class GCode(object):
 		elif cmd == "T":
 			return self.process_T(code, words)
 
-	def position_generator(self):
+	def command_generator(self):
 		for l in self.f:
 			cmd = l[0]
 			if not cmd in ['G', 'M', 'T']:
@@ -63,7 +88,7 @@ class GCode(object):
 			ret = self.process_line(cmd, l[1:].strip(" \r\n"))
 			if ret is not None:
 				yield ret
-		f.close()
+		self.f.close()
 		raise StopIteration
 
 if __name__ == "__main__":
--- move.py
+++ move.py
@@ -12,38 +12,59 @@ class Move(object):
 		self.mm2steps = [float(x) for x in cfg.settings["steps_per_mm"]]
 		self.dim = cfg.settings["num_motors"]
 		self.motor_name = cfg.settings["motor_name"]
-		self.plist = [[100, 80, 50, 20], [-10, -100, 50, 20], [-10, 20, 30, 40], [0, 0, 0, 0]]
+		self.motor_name_indexes = {}
+		for i in range(len(self.motor_name)):
+			self.motor_name_indexes[self.motor_name[i]] = i
+		#self.plist = [[100, 80, 50, 20], [-10, -100, 50, 20], [-10, 20, 30, 40], [0, 0, 0, 0]]
 		self.gcode = gcode
+		self.movements = self.movement_generator()
 
 	def transform(self, gpos):
 		ret = map(lambda x, y: x * y, gpos, self.mm2steps)
 		return ret
 
-	def position(self):
+	def movement_generator(self):
 		#while True:
 		#	for p in self.plist:
 		#		yield self.transform(p)
 		while True:
 			try:
-				gpos = next(self.gcode.position)
+				obj = next(self.gcode.commands)
 			except StopIteration:
 				break
-			p = self.transform(gpos)
-			yield p
+			if not "command" in obj:
+				print "MOVE: Onknown command object:", repr(obj)
+				continue
+			cmd = obj["command"]
+			if cmd == "position":
+				pos = [0] * self.dim
+				for w in obj:
+					idx = self.motor_name_indexes.get(w, None)
+					if idx is not None:
+						pos[idx] = obj[w]
+					elif w == "F":
+						yield ("feedrate", obj[w])
+				p = self.transform(pos)
+				yield ("position", p)
+		raise StopIteration
 
 if __name__ == "__main__":
 	from config import Config
 	from stepper import StepperCluster
 
 	cfg = Config("bla.cfg")
-	g = GCode(cfg, "enchufe.gcode")
+	gname = "enchufe.gcode"
+	if len(sys.argv) > 1:
+		gname = sys.argv[1]
+	g = GCode(cfg, gname)
 	m = Move(cfg, g)
 	n = cfg.settings["num_motors"]
 	snd = cfg.settings["sound_device"]
-	sc = StepperCluster(snd, n, m)
+	sc = StepperCluster(snd, n, cfg, m)
 	def signal_handler(signal, frame):
 		print('You pressed Ctrl+C!')
 		sc.zero_output()
+		sc.close()
 		sys.exit(0)
 	signal.signal(signal.SIGINT, signal_handler)
 	sc.main_loop()
