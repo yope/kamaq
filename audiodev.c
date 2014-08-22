a13ca0d78819508409ef6e645b4119373a94327a
David Jander <djander@gmail.com>
2014-08-22 19:23:37 +0200

    Added copyright header and licensing indication
    Signed-off-by: David Jander <djander@gmail.com>

--- audiodev.c
+++ audiodev.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2014 David Jander
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
--- gcode.py
+++ gcode.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 #
+# Copyright (c) 2014 David Jander
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
 # vim: set tabstop=4:
 
 from math import *
