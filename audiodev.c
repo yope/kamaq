fe58bd1e70f338bee2f0f9b72d3dae4c3549aa95
David Jander <djander@gmail.com>
2015-01-16 06:38:27 +0100

    audiodev.c: Increase audio buffer size.
    The new web-server seems to increase latency a bit, and audio buffer
    underruns started to occur. Need to investigate further.
    For now, increasig the buffer from 10 to 20ms seems to avoid the problem.
    
    Signed-off-by: David Jander <djander@gmail.com>

--- audiodev.c
+++ audiodev.c
@@ -112,7 +112,7 @@ int audiostep_open(const char *devname, int channels, unsigned int rate)
 	int dir = 0;
 	int i;
 	snd_pcm_uframes_t periods = PERIODSIZE;
-	snd_pcm_uframes_t bufsize = PERIODSIZE * 4;
+	snd_pcm_uframes_t bufsize = PERIODSIZE * 8;
 
 	dim = channels;
 
